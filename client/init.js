eb0fa2f21324e44e5bc31ea823d29e0bd116ed07
Danilo Paez en Mac <danilo@epy.me>
2016-11-09 12:50:45 -0300

    some files to PWA

--- client/init.js
+++ client/init.js
@@ -1,3 +1,11 @@
 Meteor.startup(function () {
 	Meteor.subscribe("contacts");
+
+	if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+          console.log('ServiceWorker registration successful with scope:',  registration.scope);
+        }).catch(function(error) {
+          console.log('ServiceWorker registration failed:', error);
+        });
+      }
 });
